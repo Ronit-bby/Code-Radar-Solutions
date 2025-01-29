275db8a6c9d5d8680148f6374fe3073aa391484b
RONIT KHANNA <ronit.khanna420x@gmail.com>
2025-01-29 12:38:59 +0530

    Solution to Check Opposite Signs

---
+++ Check Opposite Signs@@67882a731b4392d8fc1577e1/code.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d %d", &a, &b);
+    
+    // Check if a and b have opposite signs using XOR
+    if ((a ^ b) < 0) 
+    {
+        printf("True\n");
+    }
+    else 
+    {
+        printf("False\n");
+    }
+    return 0;
+}
